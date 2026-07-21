6af862d291aa58d01bea6f8d22ada8c60c8734ca
TaylorAMort <taylor.tomlinson1396@gmail.com>
2026-07-21 15:26:52 -0600

    creation and basic html

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TaylorAMort/chirpyserver.git
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	err := server.ListenAndServe(); if err != nil {
+		log.Fatal(err)
+	}
+}
